ca8b4529358313e820ff1c0b54525467372f9f9b
Travis Smith <Travis Smith@3bff208f-0ed7-9a46-89ed-33be92d3b4da>
2010-09-09 22:59:44 +0000

    Updated Settings
    git-svn-id: svn:///M%3A/One%20Solution%20CMS%20Subversion%20Files/One%20Solution%20CMS%202.1@108 3bff208f-0ed7-9a46-89ed-33be92d3b4da

--- TravisNapoleanSmith/test.php
+++ TravisNapoleanSmith/test.php
@@ -13,9 +13,8 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 
 	// Includes all files
 	require_once ('Configuration/includes.php');
-	
-	//$credentaillogonarray = Array ('mysql', 'yroot', 'gmt461z020414', 'Test');
-	//$credentaillogonarray = Array ('mysql', 'yroot', 'gmt461z020414', 'TravisNapoleanSmithVersion2');
+
+
 	//$testdatabase = new MySqlConnect();
 	//$testdatabase->setDatabaseAll ($credentaillogonarray[0], $credentaillogonarray[1], $credentaillogonarray[2], $credentaillogonarray[3], 'Demo');
 	//$testdatabase->setDatabaseAll ($credentaillogonarray[0], $credentaillogonarray[1], $credentaillogonarray[2], $credentaillogonarray[3], 'Flash');
@@ -35,7 +34,7 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	$rowvalue[1] = 'THIS IS A TEST';
 	$rowvalue[2] = '0000-00-00';
 	$testdatabase->createRow ($rowname, $rowvalue);
-	
+
 	$rowname[0] = 'id';
 	$rowname[1] = 'joketext';
 	$rowname[2] = 'jokedate';
@@ -51,29 +50,29 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	$rowvalue[0] = 'HELLO WORLD';
 	$rownumbername[0] = 'id';
 	$rownumber[0] = 1;
-	
+
 	$rowname[1] = 'joketext';
 	$rowvalue[1] = 'HI MY NAME IS TRAVIS SMITH';
 	$rownumbername[1] = 'id';
 	$rownumber[1] = 2;
 	*/
 	//$testdatabase->updateRow($rowname, $rowvalue, $rownumbername, $rownumber);
-	
+
 	//$testdatabase->updateRow ('joketext', 'HELLO WORLD', 'id', '1');
-	
+
 	//$fieldstring = '`DUMMYTEST` TEXT NOT NULL';
 	//$testdatabase->createField($fieldstring, 'AFTER', 'joketext');
-	
+
 	//$testdatabase->deleteField ('DUMMYTEXT');
-	
+
 	//$tablestring2[0] = '`jokedate` = "1994-04-01" WHERE id=1';
 	//$tablestring2[1] = '`jokedate` = "1999-04-01" WHERE id=2';
 	//$tablestring2[2] = '`jokedate` = "2009-04-01" WHERE id=3';
 	//$testdatabase->updateTable($tablestring2);
-		
+
 	//$updatefield1 = 'jokedate';
 	//$updatefield2 = 'jokedate';
-	
+
 	//$testdatabase->updateField($updatefield1, $updatefield2);
 	/*
 	$test[0][0] = 'DOG';
@@ -82,9 +81,9 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	$test[1][0] = 'WHAT';
 	$test[1][1] = 'EVER!';
 	$test[1][2] = 6;
-	
+
 	print_r($test);
-	
+
 	if (is_array($test[0])) {
 		print "THIS IS AN ARRAY\n";
 		print_r($test[0]);
@@ -110,7 +109,7 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	$rowvalue[0][0] = '1';
 	$rowvalue[0][1] = 'THIS IS A TEST';
 	$rowvalue[0][2] = '0000-00-00';
-	
+
 	$rowname[1][0] = 'id';
 	$rowname[1][1] = 'joketext';
 	$rowname[1][2] = 'jokedate';
@@ -121,12 +120,12 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	*/
 	//print_r($rowname);
 	//print_r($rowvalue);
-	
+
 	/*
 	$deleterowname[0] = 'id';
 	$deleterowname[1] = 'id';
 	$deleterowvalue[0] = '1';
-	$deleterowvalue[1] = '2';	
+	$deleterowvalue[1] = '2';
 	$testdatabase->deleterow ($deleterowname, $deleterowvalue);
 	*/
 	//$testdatabase->deleterow ('id', '1');
@@ -147,25 +146,25 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	/*
 	$updatefield1[0] = 'jokedate2';
 	$updatefield1[1] = 'joketext2';
-	
+
 	$updatefield2[0] = 'jokedate';
 	$updatefield2[1] = 'joketext';
-	
+
 	$testdatabase->updateField($updatefield1, $updatefield2);
 	*/
 	/*
 	$testprotectionlayer = new ProtectionLayer();
 	$testprotectionlayer->setModulesLocation('Modules/Tier3ProtectionLayer/');
 	$testprotectionlayer->setDatabaseAll ($credentaillogonarray[0], $credentaillogonarray[1], $credentaillogonarray[2], $credentaillogonarray[3]);
-	
+
 	$testprotectionlayer->setDatabasetable('Flash');
 	//$testprotectionlayer->ConnectAll();
 	$testprotectionlayer->Connect('Flash');
-	
+
 	$flashidnumber = Array();
 	$flashidnumber['idnumber'] = 1;
 	$flashidnumber['idnumber1'] = 1;
-	
+
 	$functionarguments = Array();
 	$functionarguments['idnumber'] = $flashidnumber;
 	$testprotectionlayer->pass('Flash', 'setDatabaseField', $functionarguments);
@@ -182,7 +181,7 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	//$testprotectionlayer->buildModules();
 	//print "DOG\n";
 	//print_r($testprotectionlayer);
-	
+
 	//$test = $testdatabase->getError(0);
 	//$test2 = $testdatabase->getErrorArray();
 	//print "$test\n";
@@ -201,7 +200,7 @@ echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3
 	//require_once 'Configuration/Tier6-ContentLayer/mainmenu.php';
 	//print_r($Tier4Databases);
 	//print_r($calendartable);
-	
+
 /*phpinfo();
 
 
