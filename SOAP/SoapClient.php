aecffef9fced7c9bb159cdd7364153d857b656d5
Travis Smith <Travis Smith@3bff208f-0ed7-9a46-89ed-33be92d3b4da>
2010-11-04 02:32:58 +0000

    Removed SOAP from the root folder.  It was a mistake to leave it in their.
    git-svn-id: svn:///M%3A/One%20Solution%20CMS%20Subversion%20Files/One%20Solution%20CMS%202.1@117 3bff208f-0ed7-9a46-89ed-33be92d3b4da

--- SOAP/SoapClient.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$Client = new SoapClient(NULL, array('location' => 'http://kcphotovideoversion1.travisnapoleansmith.com/SOAP/SoapServer.php', 'uri' => 'http://dummy.com', 'soap_version' => SOAP_1_2));
-	$return = $Client->getHeader('DUMMYTEXT', 'This is me!');
-	//$return->endDocument();
-	//$output = $return->flush();
-	//print_r ($output);
-	var_dump($return);
-?>
